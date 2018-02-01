8884d76aa2ff460de6c32eb0b46c12f74413f7a6
ahmedu007 <umair170@hotmail.com>
2018-02-01 18:25:32 +0000

    wiring with auth0

---
+++ serverless/js/config.js
@@ -0,0 +1,6 @@
+const configConstants = {
+  auth0: {
+    domain: "24-hour-video.eu.auth0.com",
+    clientId: "4L5co8uEU9Py32fYsJx2aWg7SLw6_rJF"
+  }
+};
---
+++ serverless/js/user-controller.js
@@ -0,0 +1,85 @@
+const userController = {
+  data: {
+    auth0Lock: null,
+    config: null
+  },
+  uiElements: {
+    loginButton: null,
+    logoutButton: null,
+    profileButton: null,
+    profileNameLabel: null,
+    profileImage: null
+  },
+  init: function(config) {
+    const that = this;
+    this.uiElements.loginButton = $("#auth0-login");
+    this.uiElements.logoutButton = $("#auth0-logout");
+    this.uiElements.profileButton = $("#user-profile");
+    this.uiElements.profileNameLabel = $("#profilename");
+    this.uiElements.profileImage = $("#profilepicture");
+    this.data.config = config;
+    this.data.auth0Lock = new Auth0Lock(
+      config.auth0.clientId,
+      config.auth0.domain
+    );
+    const idToken = localStorage.getItem("userToken");
+    if (idToken) {
+      this.configureAuthenticatedRequests();
+      this.data.auth0Lock.getProfile(idToken, function(err, profile) {
+        if (err) {
+          return alert(
+            "There was an error getting the profile: " + err.message
+          );
+        }
+        that.showUserAuthenticationDetails(profile);
+      });
+    }
+    this.wireEvents();
+  },
+  configureAuthenticatedRequests: function() {
+    $.ajaxSetup({
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader(
+          "Authorization",
+          "Bearer " + localStorage.getItem("userToken")
+        );
+      }
+    });
+  },
+  showUserAuthenticationDetails: function(profile) {
+    const showAuthenticationElements = !!profile;
+    if (showAuthenticationElements) {
+      this.uiElements.profileNameLabel.text(profile.nickname);
+      this.uiElements.profileImage.attr("src", profile.picture);
+    }
+
+    this.uiElements.loginButton.toggle(!showAuthenticationElements);
+    this.uiElements.logoutButton.toggle(showAuthenticationElements);
+    this.uiElements.profileButton.toggle(showAuthenticationElements);
+  },
+  wireEvents: function() {
+    const that = this;
+    this.uiElements.loginButton.click(function(e) {
+      const params = {
+        authParams: {
+          scope: "openid email user_metadata picture"
+        }
+      };
+      that.data.auth0Lock.show(params, function(err, profile, token) {
+        if (err) {
+          alert("There was an error");
+        } else {
+          localStorage.setItem("userToken", token);
+          that.configureAuthenticatedRequests();
+          that.showUserAuthenticationDetails(profile);
+        }
+      });
+    });
+    this.uiElements.logoutButton.click(function(e) {
+      localStorage.removeItem("userToken");
+      that.uiElements.logoutButton.hide();
+      that.uiElements.profileButton.hide();
+      that.uiElements.loginButton.show();
+    });
+  }
+};

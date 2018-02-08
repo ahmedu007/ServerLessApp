d916f40de23b4280657ef32054f37a4f35c2341a
ahmedu007 <umair170@hotmail.com>
2018-02-08 12:02:39 +0000

    fetching list of all the videos in bucket

--- get-video-list/index.js
+++ get-video-list/index.js
@@ -1,14 +1,32 @@
-"use strict";
+("use strict");
 
 const AWS = require("aws-sdk");
 const async = require("async");
-const email = require("./email");
-
 const s3 = new AWS.S3();
+
+function createErrorResponse(code, message, encoding) {
+  var response = {
+    statusCode: code,
+    headers: { "Access-Control-Origin": "*" },
+    body: JSON.stringify({ code, message, encoding })
+  };
+
+  return response;
+}
+
+function createSuccessResponse(result) {
+  var response = {
+    statusCode: 200,
+    headers: { "Access-Control-Origin": "*" },
+    body: JSON.stringify(result)
+  };
+
+  return response;
+}
+
 function createBucketParams(next) {
   var params = {
-    Bucket: process.env.BUCKET,
-    EncodingType: "url"
+    Bucket: process.env.BUCKET
   };
   next(null, params);
 }
@@ -23,30 +41,60 @@ function getVideosFromBucket(params, next) {
   });
 }
 
-function createList(data, next) {
-  var urls = [];
+function createList(encoding, data, next) {
+  var files = [];
   for (var i = 0; i < data.Contents.length; i++) {
     var file = data.Contents[i];
-    if (file.Key && file.Key.substr(-3, 3) === "mp4") {
-      urls.push(file);
+
+    if (encoding) {
+      var type = file.Key.substr(file.Key.lastIndexOf("-") + 1);
+      if (type !== encoding + ".mp4") {
+        continue;
+      } else {
+        if (file.Key.slice(-4) !== ".mp4") {
+          continue;
+        }
+      }
     }
+
+    files.push({
+      filename: file.Key,
+      eTag: file.ETag.replace(/"/g, ""),
+      size: file.Size
+    });
   }
+
   var result = {
     baseUrl: process.env.BASE_URL,
     bucket: process.env.BUCKET,
-    urls: urls
+    files: files
   };
   next(null, result);
 }
 
 exports.handler = function(event, context, callback) {
+  var encoding = null;
+  if (event.queryStringParameters && event.queryStringParameters.encoding) {
+    encoding = decodeURIComponent(event.queryStringParameters.encoding);
+  }
   async.waterfall(
-    [createBucketParams, getVideosFromBucket, createList],
+    [
+      createBucketParams,
+      getVideosFromBucket,
+      async.apply(createList, encoding)
+    ],
     function(err, result) {
       if (err) {
-        callback(err);
+        callback(null, createErrorResponse(500, err, encoding));
       } else {
-        callback(null, result);
+        if (result.files.length > 0) {
+          callback(null, createSuccessResponse(result));
+        } else {
+          callback(
+            null,
+            createErrorResponse(404, "No files were found", encoding)
+          );
+        }
       }
     }
   );
